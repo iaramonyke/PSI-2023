a92c839f2a7e58dcafbc9fa5473eea85afd7ddb1
Iara Monyke <i.monyke@academico.ifrn.edu.br>
2023-07-31 13:26:20 -0300

    finalizado

---
+++ WebAppProjeto2023/WebAppProjeto2023/Controllers/ProdutosController.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppProjeto2023.Models;
+using System.Data.Entity;
+using System.Net;
+using System.IO;
+using Modelo.Cadastros;
+using Servico.Cadastros;
+using Servico.Tabelas;
+
+namespace WebAppProjeto2023.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        //private EFContext context = new EFContext();
+        private ProdutoServico produtoServico = new ProdutoServico();
+        private CategoriaServico categoriaServico = new CategoriaServico();
+        private FabricanteServico fabricanteServico = new FabricanteServico();
+
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            //var produtos = context.Produtos.Include(c => c.Categoria). // Acesso ao contexto
+            // Include(f => f.Fabricante).OrderBy(n => n.Nome); // (comentado)
+            //return View(produtos);
+            return View(produtoServico.ObterProdutosClassificadosPorNome());
+        }
+
+        private ActionResult ObterVisaoProdutoPorId(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = produtoServico.ObterProdutoPorId((long)id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // GET: Produtos/Details/5
+        public ActionResult Details(long? id)
+        {
+            return ObterVisaoProdutoPorId(id);
+        }
+
+        // GET: Produtos/Create
+        public ActionResult Create()
+        {
+            PopularViewBag();
+            return View();
+        }
+
+        // POST: Produtos/Create
+        [HttpPost]
+        public ActionResult Create(Produto produto)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                //context.Produtos.Add(produto);
+                //context.SaveChanges();
+                produtoServico.GravarProduto(produto);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(produto);
+            }
+        }
+
+        // GET: Produtos/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            PopularViewBag(produtoServico.ObterProdutoPorId((long)id));
+            return ObterVisaoProdutoPorId(id);
+        }
+
+        private byte[] SetLogotipo(HttpPostedFileBase logotipo)
+        {
+            var bytesLogotipo = new byte[logotipo.ContentLength];
+            logotipo.InputStream.Read(bytesLogotipo, 0, logotipo.ContentLength);
+            return bytesLogotipo;
+        }
+
+        // Metodo Privado
+        private void PopularViewBag(Produto produto = null)
+        {
+            if (produto == null)
+            {
+                ViewBag.CategoriaId = new SelectList(categoriaServico.ObterCategoriasClassificadasPorNome(),
+                "CategoriaId", "Nome");
+                ViewBag.FabricanteId = new SelectList(fabricanteServico.ObterFabricantesClassificadosPorNome(),
+                "FabricanteId", "Nome");
+            }
+            else
+            {
+                ViewBag.CategoriaId = new SelectList(categoriaServico.ObterCategoriasClassificadasPorNome(),
+                "CategoriaId", "Nome", produto.CategoriaId);
+                ViewBag.FabricanteId = new SelectList(fabricanteServico.ObterFabricantesClassificadosPorNome(),
+                "FabricanteId", "Nome", produto.FabricanteId);
+            }
+        }
+
+        /* public void GravarProduto(Produto produto)
+        {
+            if (produto.ProdutoId == null)
+            {
+                context.Produtos.Add(produto);
+            }
+            else
+            {
+                context.Entry(produto).State = EntityState.Modified;
+            }
+            context.SaveChanges();
+        } */
+
+        private ActionResult GravarProduto(Produto produto, HttpPostedFileBase logotipo, string chkRemoverImagem)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (chkRemoverImagem != null)
+                    {
+                        produto.Logotipo = null;
+                    }
+                    if (logotipo != null)
+                    {
+                        produto.LogotipoMimeType = logotipo.ContentType;
+                        produto.Logotipo = SetLogotipo(logotipo);
+                    }
+                    produtoServico.GravarProduto(produto);
+                    return RedirectToAction("Index");
+                }
+                PopularViewBag(produto);
+                return View(produto);
+            }
+            catch
+            {
+                PopularViewBag(produto);
+                return View(produto);
+            }
+        }
+
+        public Produto ObterProdutoPorId(long id)
+        {
+            Produto produto = produtoServico.ObterProdutoPorId(id);
+            return produto;
+            //return context.Produtos.Where(p => p.ProdutoId == id).Include(c => c.Categoria).Include(f => f.Fabricante).First();
+        }
+
+        public FileContentResult GetLogotipo(long id)
+        {
+            Produto produto = ObterProdutoPorId(id);
+            if (produto != null)
+            {
+                return File(produto.Logotipo, produto.LogotipoMimeType);
+            }
+            return null;
+        }
+
+        public FileContentResult GetLogotipo2(long id)
+        {
+            Produto produto = ObterProdutoPorId(id);
+            if (produto != null)
+            {
+                if (produto.NomeArquivo != null)
+                {
+                    var bytesLogotipo = new byte[produto.TamanhoArquivo];
+                    FileStream fileStream = new
+                    FileStream(Server.MapPath("~/App_Data/" + produto.NomeArquivo), FileMode.Open,
+                    FileAccess.Read);
+                    fileStream.Read(bytesLogotipo, 0, (int)produto.TamanhoArquivo);
+                    return File(bytesLogotipo, produto.LogotipoMimeType);
+                }
+            }
+            return null;
+        }
+
+        public ActionResult DownloadArquivo(long id)
+        {
+            Produto produto = ObterProdutoPorId(id);
+            FileStream fileStream = new FileStream(Server.MapPath("~/App_Data/" + produto.NomeArquivo), FileMode.Create,FileAccess.Write);
+            fileStream.Write(produto.Logotipo, 0,Convert.ToInt32(produto.TamanhoArquivo));
+            fileStream.Close();
+            return File(fileStream.Name, produto.LogotipoMimeType, produto.NomeArquivo);
+        }
+
+        public ActionResult DownloadArquivo2(long id)
+        {
+
+            Produto produto = ObterProdutoPorId(id);
+            FileStream fileStream = new FileStream(Server.MapPath("~/App_Data/" +
+            produto.NomeArquivo), FileMode.Open, FileAccess.Read);
+            return File(fileStream.Name, produto.LogotipoMimeType, produto.NomeArquivo);
+
+        }
+
+        // POST: Produtos/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Produto produto,HttpPostedFileBase logotipo = null, string chkRemoverImagem = null)
+        {
+            return GravarProduto(produto, logotipo, chkRemoverImagem);
+        }
+
+        // GET: Produtos/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            return ObterVisaoProdutoPorId(id);
+        }
+
+        // POST: Produtos/Delete/5
+        [HttpPost]
+        public ActionResult Delete(long id)
+        {
+            try
+            {
+                //Produto produto = context.Produtos.Find(id);
+                //context.Produtos.Remove(produto);
+                //context.SaveChanges();
+                Produto produto = produtoServico.EliminarProdutoPorId(id);
+                TempData["Message"] = "Produto \"" + produto.Nome + "\" foi removido";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
